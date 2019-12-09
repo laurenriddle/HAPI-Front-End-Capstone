c919f822c3391afb8c5d46f5f967f71ae55a5b61
Lauren Riddle <lari7132@gmail.com>
2019-12-09 17:43:26 -0600

    added conditional rendering to navbar

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -6,22 +6,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Navbar extends Component {
-    render () {
-        return (
-<>
-  <NavBar bg="light" variant="light">
-    <NavBar.Brand href="#home"> <img src={require('./Logo.png')} width="100" height="40"/></NavBar.Brand>
-    <Nav className="mr-auto">
-     <Link className="nav-link" to="/">Home</Link>
-     <Link className="nav-link" to="/resources">Resources</Link>
-     <Link className="nav-link" to="/apisearch">API Search</Link>
-     <Link className="nav-link" to="/login">Login</Link>
-     <Link className="nav-link" to="/Register">Register</Link>
-     <Link className="nav-link nav-align-right" to="/Login" onClick={this.props.clearUser}>Logout</Link>
-    </Nav>
-  </NavBar>
-</>
-) }
+  render() {
+    if (this.props.user === true) {
+      return (
+        <>
+          <NavBar bg="light" variant="light">
+            <NavBar.Brand href="#home"> <img src={require('./Logo.png')} width="100" height="40" /></NavBar.Brand>
+            <Nav className="mr-auto">
+              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/resources">Resources</Link>
+              <Link className="nav-link" to="/apisearch">API Search</Link>
+              <Link className="nav-link nav-align-right" to="/Login" onClick={this.props.clearUser}>Logout</Link>
+            </Nav>
+          </NavBar>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <NavBar bg="light" variant="light">
+            <NavBar.Brand href="#home"> <img src={require('./Logo.png')} width="100" height="40" /></NavBar.Brand>
+            <Nav className="mr-auto">
+              <Link className="nav-link" to="/login">Login</Link>
+              <Link className="nav-link" to="/Register">Register</Link>
+            </Nav>
+          </NavBar>
+        </>
+      )
+    }
+  }
 }
 
 export default Navbar
