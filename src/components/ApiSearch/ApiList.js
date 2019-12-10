a2f2e0f4c4d4e3d84bffd5e80174c167223c631e
Lauren Riddle <lari7132@gmail.com>
2019-12-10 10:34:26 -0600

    got external API search working correctly

--- src/components/ApiSearch/ApiList.js
+++ src/components/ApiSearch/ApiList.js
@@ -3,7 +3,7 @@ import { Button, Form } from 'react-bootstrap';
 import ExternalAPIManager from '../../modules/ExternalAPIManager';
 import ListCard from './ListCards';
 
-
+let results = []
 class ApiList extends Component {
     state = {
         results: [],
@@ -29,16 +29,54 @@ class ApiList extends Component {
         console.log(this.state.terms)
     }
 
+    pushEntry = (array, item) => {
+        if (!array.find(({Link}) => Link === item.Link)) {
+          array.push(item);
+        }
+      }
+
     searchExternalApi = userInput => {
         console.log(this.state.terms)
+        results = []
         ExternalAPIManager.searchByDescription(this.state.terms)
-            .then((Response) => {
-                this.setState({
-                    results: Response.entries
+            .then((response) => {
+                console.log(response.entries)
+                if(response.entries !== null) {
+                console.log(response.entries)
+                response.entries.forEach(entry => {
+                    results.push(entry)
                 })
+            }
                 console.log(this.state.results)
+            }).then( () => {
+        ExternalAPIManager.searchByCategory(this.state.terms)
+        .then(response => {
+
+            if(response.entries !== null) {
+            response.entries.forEach(entry => {
+                this.pushEntry(results, entry)
+                // results.push(entry)
             })
+        }
+        })
+     }).then(() => {
+        ExternalAPIManager.searchByTitle(this.state.terms)
+        .then(response => {
+            if(response.entries !== null) {
+            response.entries.forEach(entry => {
+                this.pushEntry(results, entry)
+
+                // results.push(entry)
+               
+            })
+        }
+                this.setState({
+                    results: results
+                })
+        })
+    })
     }
+
     render() {
         if (this.state.results === null) {
 
@@ -75,14 +113,18 @@ class ApiList extends Component {
                     <div className="erd-container-cards">
                         <hr /><h2>Search Results:</h2><hr />
                         {
-                            this.state.results.map((result) => {
-                                // if the index of the event is equal to 0, render the card with the bold text and background color
-                               console.log(result)
+                            this.state.results.map((result, index) => {
+
+                                if(index < 20) {
+                                if(result.Link !== result.Link)
+                               console.log("map",result)
+                               console.log(index)
                                 return <ListCard
                                     key={result.Link}
                                     result={result}
                                     {...this.props}
                                 />
+                                }
                             })}
                     </div>
                 </>
--- src/modules/ExternalAPIManager.js
+++ src/modules/ExternalAPIManager.js
@@ -5,5 +5,12 @@ export default {
   searchByDescription(userInput) {
     return fetch(`https://api.publicapis.org/entries?description=${userInput}`)
       .then(data => data.json());
+  }, searchByTitle(userInput) {
+    return fetch(`https://api.publicapis.org/entries?title=${userInput}`)
+      .then(data => data.json());
+  }, searchByCategory(userInput) {
+    return fetch(`https://api.publicapis.org/entries?category=${userInput}`)
+      .then(data => data.json());
   }
+
 }
