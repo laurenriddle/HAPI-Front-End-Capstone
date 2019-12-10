758cafed6104f798d5bd5a0817934627c21b1f9d
Lauren Riddle <lari7132@gmail.com>
2019-12-10 11:35:32 -0600

    fixed external API search

--- src/components/ApiSearch/ApiList.js
+++ src/components/ApiSearch/ApiList.js
@@ -19,106 +19,104 @@ class ApiList extends Component {
     }
 
     pushEntry = (array, item) => {
-        if (!array.find(({Link}) => Link === item.Link)) {
-          array.push(item);
+        if (!array.find(({ Link }) => Link === item.Link)) {
+            array.push(item);
         }
-      }
+    }
 
     searchExternalApi = userInput => {
-        console.log(this.state.terms)
         results = []
         ExternalAPIManager.searchByDescription(this.state.terms)
             .then((response) => {
-                console.log(response.entries)
-                if(response.entries !== null) {
-                console.log(response.entries)
-                response.entries.forEach(entry => {
-                    results.push(entry)
-                })
-            }
-                console.log(this.state.results)
-            }).then( () => {
+                if (response.entries !== null) {
+                    response.entries.forEach(entry => {
+                        this.pushEntry(results, entry)
+                    })
+                    // console.log("description", results)
+                }
+            })
         ExternalAPIManager.searchByCategory(this.state.terms)
-        .then(response => {
+            .then(response => {
 
-            if(response.entries !== null) {
-            response.entries.forEach(entry => {
-                this.pushEntry(results, entry)
-                // results.push(entry)
+                if (response.entries !== null) {
+                    response.entries.forEach(entry => {
+                        this.pushEntry(results, entry)
+                    })
+                }
+                // console.log("category", results)
+
             })
-        }
-        })
-     }).then(() => {
+
         ExternalAPIManager.searchByTitle(this.state.terms)
-        .then(response => {
-            if(response.entries !== null) {
-            response.entries.forEach(entry => {
-                this.pushEntry(results, entry)
+            .then(response => {
+                if (response.entries !== null) {
+                    response.entries.forEach(entry => {
+                        this.pushEntry(results, entry)
 
-                // results.push(entry)
-               
-            })
-        }
+                    })
+                }
+                // console.log("title", results)
+
+            }).then(() => {
                 this.setState({
-                    results: results
+                    results: results,
                 })
-        })
-    })
+                // console.log("results after set state", this.state.results)
+
+            })
     }
 
     render() {
-        if (this.state.results === null) {
+        // if (this.state.results.length === 0) {
 
-            return (
-                <>
-                    <section className="section-content">
-                        <Form>
-                            <Form.Group>
-                                <Form.Label>Search for APIs:</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Search Terms" id="terms" onChange={this.handleFieldChange} />
-                            </Form.Group>
-                            <Button disabled={this.state.loadingStatus} onClick={this.searchExternalApi}>Search</Button>
-                        </Form>
-                    </section>
-                    <div className="erd-container-cards">
-                        <hr /><h2>Search Results:</h2><hr />
-                        <h3>No search results</h3>
-                    </div>
-                </>
-            )
-        }
-        else {
-            return (
-                <>
-                    <section className="section-content">
-                        <Form>
-                            <Form.Group>
-                                <Form.Label><h3>Search for APIs:</h3></Form.Label>
-                                <Form.Control type="text" placeholder="Enter Search Terms" id="terms" onChange={this.handleFieldChange} />
-                            </Form.Group>
-                            <Button disabled={this.state.loadingStatus} onClick={this.searchExternalApi}>Search</Button>
-                        </Form>
-                    </section>
-                    <div className="erd-container-cards">
-                        <hr /><h2>Search Results:</h2><hr />
-                        {
-                            this.state.results.map((result, index) => {
+        //     return (
+        //         <>
+        //             <section className="section-content">
+        //                 <Form>
+        //                     <Form.Group>
+        //                         <Form.Label>Search for APIs:</Form.Label>
+        //                         <Form.Control type="text" placeholder="Enter Search Terms" id="terms" onChange={this.handleFieldChange} />
+        //                     </Form.Group>
+        //                 </Form>
+        //                 <Button disabled={this.state.loadingStatus} onClick={this.searchExternalApi}>Search</Button>
+        //             </section>
+        //             <div className="erd-container-cards">
+        //                 <hr /><h2>Search Results:</h2><hr />
+        //                 <h3>No search results</h3>
+        //             </div>
+        //         </>
+        //     )
+        // }
+        // else {
+        return (
+            <>
+                <section className="section-content">
+                    <Form>
+                        <Form.Group>
+                            <Form.Label><h3>Search for APIs:</h3></Form.Label>
+                            <Form.Control type="text" placeholder="Enter Search Terms" id="terms" onChange={this.handleFieldChange} />
+                        </Form.Group>
+                        <Button disabled={this.state.loadingStatus} onClick={this.searchExternalApi}>Search</Button>
+                    </Form>
+                </section>
+                <div className="erd-container-cards">
+                    <hr /><h2>Search Results:</h2><hr />
+                    {
+                        this.state.results.map((result, index) => {
 
-                                if(index < 20) {
-                               console.log("map",result)
-                               console.log(index)
-                                return <ListCard
-                                    key={result.Link}
-                                    result={result}
-                                    {...this.props}
-                                />
-                                }
-                            })}
-                    </div>
-                </>
-            )
-        }
+                            // if (index < 20) {
+                            return <ListCard
+                                key={result.Link}
+                                result={result}
+                                {...this.props}
+                            />
+                            // }
+                        })}
+                </div>
+            </>
+        )
     }
+    // }
 }
 
 
