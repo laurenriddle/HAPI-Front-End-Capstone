ad70ac7ceab270fc73de416c9df5c083bdb8bc07
Lauren Riddle <lari7132@gmail.com>
2019-12-09 18:46:46 -0600

    completed POST and PUT and DELETE on erds

--- src/components/ApplicationView.js
+++ src/components/ApplicationView.js
@@ -3,54 +3,67 @@ import React, { Component } from "react";
 import LogIn from "./Auth/Login";
 import Register from "./Auth/Registration";
 import ResourceList from "./Resources/ResourceList";
+import ErdForm from "./Resources/ErdForm";
+import ErdEditForm from "./Resources/ErdEditForm";
 
 export default class ApplicationViews extends Component {
 
-    render() {
-      return (
-        <React.Fragment>
-  
-  
-          <Route
-            exact path="/login" render={props => {
-              // console.log("app view", this.props.user)
-              if (this.props.user) {
-  
-              return <Redirect to="/" />
-              } else {
-              return <LogIn setUser={this.props.setUser} searchUsers={this.props.searchUsers}
-               {...props} {...this.props} />
-              }}}
-          />
-           <Route
-            exact path="/Register" render={props => {
-              // console.log("app view", this.props.user)
-              if (this.props.user) {
-  
-              return <Redirect to="/" />
-              } else {
-              return <Register setUser={this.props.setUser} searchUsers={this.props.searchUsers}
-               {...props} {...this.props} />
-              }}}
-          />
-          <Route
-            exact exact path="/Resources" render={props => {
-              // console.log("app view", this.props.user)
-             
-              return <ResourceList {...props} />
-              }}
-          />
+  render() {
+    return (
+      <React.Fragment>
 
-          {/* <Route
+
+        <Route
+          exact path="/login" render={props => {
+            // console.log("app view", this.props.user)
+            if (this.props.user) {
+
+              return <Redirect to="/" />
+            } else {
+              return <LogIn setUser={this.props.setUser} searchUsers={this.props.searchUsers}
+                {...props} {...this.props} />
+            }
+          }}
+        />
+        <Route
+          exact path="/Register" render={props => {
+            // console.log("app view", this.props.user)
+            if (this.props.user) {
+
+              return <Redirect to="/" />
+            } else {
+              return <Register setUser={this.props.setUser} searchUsers={this.props.searchUsers}
+                {...props} {...this.props} />
+            }
+          }}
+        />
+        <Route
+          exact exact path="/Resources" render={props => {
+            // console.log("app view", this.props.user)
+
+            return <ResourceList {...props} />
+          }}
+        />
+
+        <Route path="/erd/new" render={(props) => {
+         return <ErdForm {...props} {...this.props} />
+
+        }} />
+          <Route
+          path="/erds/:erdId(\d+)/edit" render={props => {
+            return <ErdEditForm {...props} {...this.props} />
+          }}
+        />
+        {/* <Route
             exact path="/" render={props => {
               
               return <Home />
               }}
           /> */}
 
-  
-  
-        </React.Fragment>
-      );
-    }
+
+
+      </React.Fragment>
+    );
   }
+}
---
+++ src/components/Resources/ErdEditForm.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react"
+import APIManager from "../../modules/APIManager";
+import { Form, Button } from 'react-bootstrap';
+
+
+class ErdEditForm extends Component {
+    //set the initial state
+    state = {
+        name: "",
+        notes: "",
+        link: "",
+        loadingStatus: false,
+    };
+
+    handleFieldChange = evt => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    updateExistingErd = evt => {
+        evt.preventDefault()
+        this.setState({ loadingStatus: true });
+        const currentUser = JSON.parse(localStorage.getItem("credentials"))
+
+
+        const editedErd = {
+            name: this.state.name,
+            notes: this.state.notes,
+            link: this.state.link,
+            userId: currentUser.id,
+            id: this.props.match.params.erdId
+        }
+
+        APIManager.update("erds", editedErd)
+            .then(() => this.props.history.push("/Resources"))
+    }
+
+    componentDidMount() {
+        APIManager.get(`erds/${this.props.match.params.erdId}`)
+            .then(erd => {
+                if (Object.keys(erd).length === 0) {
+                    this.props.history.push("/events")
+                    window.alert('The event you were trying to access does not exists.')
+                } else {
+                    this.setState({
+                        name: erd.name,
+                        notes: erd.notes,
+                        link: erd.link,
+                        loadingStatus: false,
+                    });
+                }
+            });
+    }
+
+    render() {
+        return (
+            <>
+                <div id="erdEditForm">
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Name:</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Name" id="name" value={this.state.name} onChange={this.handleFieldChange} />
+                            <Form.Label>Notes:</Form.Label>
+                            <Form.Control type="date" id="notes" value={this.state.notes} onChange={this.handleFieldChange} />
+                            <Form.Label>Link:</Form.Label>
+                            <Form.Control type="text" id="link" value={this.state.link} onChange={this.handleFieldChange} />
+                        </Form.Group>
+                        <Button
+                            type="button"
+                            disabled={this.state.loadingStatus}
+                            onClick={this.updateExistingErd}
+                        >Save</Button>
+                    </Form>
+                </div>
+            </>
+        );
+    }
+}
+
+export default ErdEditForm
